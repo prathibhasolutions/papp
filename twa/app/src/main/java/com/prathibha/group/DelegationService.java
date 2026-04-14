37009be118f05d1601935ebcfa53ee524f314d32
prathibhasolutions <148753496+prathibhasolutions@users.noreply.github.com>
2026-04-14 16:41:50 +0530

    M

---
+++ twa/app/src/main/java/com/prathibha/group/DelegationService.java
@@ -0,0 +1,14 @@
+package com.prathibha.group;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
